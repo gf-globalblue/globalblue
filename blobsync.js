bbba05a19068813e09fc206daf8ad1bd617a3793
Georg Frysak <gfrysak@globalblue.com>
2025-09-16 13:48:30 +0200

    fixed blobsync not being able to link code to the sync button

--- blobsync.js
+++ blobsync.js
@@ -1,7 +1,7 @@
 //fetch("https://raw.githubusercontent.com/gf-globalblue/globalblue/refs/heads/dev/blobsync.js").then(r=>r.text().then(t=>(async f=>0).constructor(t)(r)));
 if (! window.require) await fetch(new URL(".", arguments[0].url).href + "modloader.js").then(r=>r.text().then(t=>new Function(t)(r)));
 document.head.remove();
-document.body.outerHTML = '<body><input type="button" value="sync" onclick="syncClick()"><pre class="content-area" id="log" style="overflow-y:scroll; background-color:#EEEEEE; height: calc(100vh - 50px)"></pre></body>'
+document.body.outerHTML = '<body><input id="syncButton" type="button" value="sync"><pre class="content-area" id="log" style="overflow-y:scroll; background-color:#EEEEEE; height: calc(100vh - 50px)"></pre></body>'
 document.title = `blobsync for ${document.location.origin}`;
 if (!window.DirectorySync) {
 	(async func => {
@@ -9,7 +9,7 @@ if (!window.DirectorySync) {
 		await require("blobdb.js");
 	})();
 }
-async function syncClick() {
+document.getElementById("syncButton").onclick = async function() {
 	let logElement = document.getElementById("log");
 	window.dirSync = new DirectorySync({path: "scripts", handle: await window.showDirectoryPicker(), recurse: false, pattern: /\.js$/});
 	window.dirSync.cbLog = msg => logElement.prepend(`${new Date().toISOString()} ${msg}\n`);
