7897478d44f733e2c1505091e01a1fb5b55431de
Georg Frysak <gfrysak@globalblue.com>
2025-09-16 11:52:48 +0200

    modified blobsync to use relative URLs for post loading the modloader

--- blobsync.js
+++ blobsync.js
@@ -1,5 +1,5 @@
 (async func => {
-	if (! window.require) await fetch("https://raw.githubusercontent.com/gf-globalblue/globalblue/refs/heads/dev/modloader.js").then(r=>r.text().then(t=>new Function(t)(r)));
+	if (! window.require) await fetch(new URL(".", arguments[0].url).href + "modloader.js").then(r=>r.text().then(t=>new Function(t)(r)));
 	await require("blobdb.js");
 	dirSync = new DirectorySync({path: "scripts", handle: await window.showDirectoryPicker(), recurse: false}); //, pattern: /\.js$/});
 	monitor = dirSync.startMonitor(3000);
