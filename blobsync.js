1df61c36119d09023a904a670ffc5f75b96f1bab
Georg Frysak <gfrysak@globalblue.com>
2025-09-16 12:40:17 +0200

    default blobsync.js to async invocation

--- blobsync.js
+++ blobsync.js
@@ -1,10 +1,8 @@
-(async func => {
-	if (! window.require) await fetch(new URL(".", arguments[0].url).href + "modloader.js").then(r=>r.text().then(t=>new Function(t)(r)));
-	await require("blobdb.js");
-	dirSync = new DirectorySync({path: "scripts", handle: await window.showDirectoryPicker(), recurse: false}); //, pattern: /\.js$/});
-	monitor = dirSync.startMonitor(3000);
-	document.body?.remove();
-	document.title = `blobsync for ${document.location.origin}`;
-})();
+if (! window.require) await fetch(new URL(".", arguments[0].url).href + "modloader.js").then(r=>r.text().then(t=>new Function(t)(r)));
+await require("blobdb.js");
+dirSync = new DirectorySync({path: "scripts", handle: await window.showDirectoryPicker(), recurse: false}); //, pattern: /\.js$/});
+monitor = dirSync.startMonitor(3000);
+document.body?.remove();
+document.title = `blobsync for ${document.location.origin}`;
 
-// fetch("https://raw.githubusercontent.com/gf-globalblue/globalblue/refs/heads/dev/blobsync.js").then(r=>r.text().then(t=>new Function(t)(r)));
+//fetch("https://raw.githubusercontent.com/gf-globalblue/globalblue/refs/heads/dev/blobsync.js").then(r=>r.text().then(t=>(async f=>0).constructor(t)(r)));
